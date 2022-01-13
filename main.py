4af063d6736f3b3dfbb5c0db84fe5868b34f45fc
dubito <94066111+dubito-tech@users.noreply.github.com>
2022-01-13 19:33:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from webbot import Browser
+from time import sleep
+web = Browser()
+print('================================')
+print('Made By: Dubito')
+print('Use Your own URL')
+print('Leave Blank For Default Website')
+print('================================')
+website = input('Website: ')
+
+if website == 'G':
+	web.go_to('https://www.google.com/')
+
+if website == '':
+	web.go_to('https://pypi.org/project/webbot')
+
+
+else:
+	web.go_to(website)
+
+while True:
+      sleep(0.1)
